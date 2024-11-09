ed81443ae831f6b9c56935d003ae16d063f198b6
bptiago <bptiago123@gmail.com>
2024-11-09 00:03:02 -0300

    Add calculo para desconto com pontos de fidelidade

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1204,9 +1204,9 @@ db.usuarios.updateMany(
 
 // Pontos de fidelidade
 function ganharPontosDeFidelidade(precoCompra, idUsuario) {
-  if (precoCompra < 20) return;
+  if (precoCompra < 20) return "Compra insuficiente para ganhar cupons!";
 
-  const pontos = Math.round(precoCompra / 10);
+  const pontos = Math.round(precoCompra / 20);
 
   const resultado = db.usuarios.updateOne(
     { id: idUsuario },
@@ -1215,8 +1215,26 @@ function ganharPontosDeFidelidade(precoCompra, idUsuario) {
   );
 
   return resultado.modifiedCount > 0
-    ? "Pontos de fidelidade atualizados com sucesso."
+    ? `${pontos} ponto(s) de fidelidade adicionado(s) para o usuário de ID ${idUsuario}`
     : `Erro ao incrementar pontos de fidelidade para o usuário de ID ${idUsuario}.`;
 }
 
-ganharPontosDeFidelidade(58, 0);
+ganharPontosDeFidelidade(60, 2);
+
+function calcularDescontoComPontosFidelidade(precoCompra, idUsuario) {
+  const usuario = db.usuarios.findOne(
+    { id: idUsuario },
+    { id: 1, email: 1, pontosFidelidade: 1 }
+  );
+
+  if (usuario == null) return `Não foi encontrado usuário com ID ${idUsuario}`;
+
+  // Cada ponto de fidelidade vale 5 reais.
+  const desconto = ((usuario.pontosFidelidade * 5) / precoCompra) * 100;
+
+  return desconto >= 100
+    ? "100% de desconto"
+    : `${desconto.toFixed(1)}% de desconto`;
+}
+
+calcularDescontoComPontosFidelidade(30, 2);
